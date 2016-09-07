a73ccbdcd79dfb305298f59d181b1dad0daa886f
wildpenguin <wildpenguin@gmail.com>
2016-09-06 23:01:58 -0400

    first commit: create symfony project

---
+++ README.md
@@ -0,0 +1,4 @@
+restaurant-food-site
+====================
+
+A Symfony project created on June 24, 2016, 4:31 am.
---
+++ src/AppBundle/Entity/SiteManager.php
@@ -0,0 +1,17 @@
+<?php 
+
+// src/AppBundle/Entity/SiteManager.php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+* @ORM\Entity
+* @ORM\Table(name="site_manager")
+*/
+
+class SiteManager 
+{
+	
+}
