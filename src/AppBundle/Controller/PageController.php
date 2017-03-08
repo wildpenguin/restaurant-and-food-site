3bfac291c8a681371c31f1f7a9813ddf480df9cd
wildpenguin <wildpenguin@gmail.com>
2017-03-07 22:22:51 -0500

    remove node_modules from repo

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -16,19 +16,9 @@ class PageController extends Controller
      */
     public function indexAction(Request $request)
     {
-        $this->registerTwigParachute();
-
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig');
     }
 
-    /**
-    * Return sample text for undefined text ids
-    */
-    public function returnSampleText()
-    {
-        return 'Sample text sample text sample text';
-    }
 
     /**
     * @Route("/webpage/text/{id}", name="postchunk")
@@ -36,24 +26,7 @@ class PageController extends Controller
     */
     public function saveTextAction(Request $request) 
     {
-
+        
     }
 
-
-    /**
-    * Defines a custom Twig function that 
-    * loads the text for given id
-    */
-    public function registerTwigParachute() 
-    {
-        $twig = new \Twig_Environment($loader);
-        $function = new \Twig_Function('parachute', function($id) {
-            $text = $this->getDoctrine()
-                ->getRepository('AppBundle:PageText')->find($id);
-            if (!$text) 
-                return $this->returnSampleText();
-            return $text->getTextContent();
-        });
-        $twig->addFunction($function);
-    }
 }
